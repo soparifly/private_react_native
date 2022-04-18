da8495456f8288ae3dc4e1064e0da0fc491706c6
DESKTOP-5E8S7BI\geniesoft <kimsojung@my.honam.ac.kr>
2022-04-18 17:21:55 +0900

    file 업로드와 uri 확인

--- app/screens/welcome/welcome-screen.tsx
+++ app/screens/welcome/welcome-screen.tsx
@@ -1,20 +1,24 @@
-import React, { FC } from "react"
-import { View, ViewStyle, TextStyle, SafeAreaView } from "react-native"
+import React, { useCallback, FC } from "react"
+import {
+  View,
+  ViewStyle,
+  TextStyle,
+  SafeAreaView,
+  Image,
+  ImageStyle,
+  Alert,
+  Linking,
+} from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
-import {
-  Button,
-  Header,
-  Screen,
-  Text,
-  GradientBackground,
-} from "../../components"
+import { Button, Header, Screen, Text, GradientBackground } from "../../components"
 import { color, spacing, typography } from "../../theme"
 import { NavigatorParamList } from "../../navigators"
 
-import ImagePicker from 'react-native-image-picker'
-import { PERMISSIONS, request, check, RESULTS } from 'react-native-permissions'
+import {} from "react-native-permissions"
 
+import ImagePicker, { launchCamera, launchImageLibrary } from "react-native-image-picker"
+// import { PERMISSIONS, request, check, RESULTS } from "react-native-permissions"
 
 const FULL: ViewStyle = { flex: 1 }
 const CONTAINER: ViewStyle = {
@@ -42,18 +46,7 @@ const HEADER_TITLE: TextStyle = {
 const TITLE_WRAPPER: TextStyle = {
   ...TEXT,
   textAlign: "center",
-  padding: 10
-}
-const CONTINUE: ViewStyle = {
-  paddingVertical: spacing[4],
-  paddingHorizontal: spacing[4],
-  backgroundColor: color.palette.deepPurple,
-}
-const CONTINUE_TEXT: TextStyle = {
-  ...TEXT,
-  ...BOLD,
-  fontSize: 13,
-  letterSpacing: 2,
+  padding: 10,
 }
 const FOOTER: ViewStyle = { backgroundColor: "#20162D" }
 const FOOTER_CONTENT: ViewStyle = {
@@ -61,86 +54,128 @@ const FOOTER_CONTENT: ViewStyle = {
   paddingHorizontal: spacing[4],
 }
 
-const includeExtra = true;
+const BUTTONSTYLEL: ViewStyle = {
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginVertical: 8,
+}
 
-export const WelcomeScreen: FC<StackScreenProps<NavigatorParamList, "welcome">> = observer(
-  ({ navigation }) => {
+const IMAGE: ImageStyle = {
+  marginVertical: 24,
+  alignItems: "center",
+}
 
+const includeExtra = true
+export function ImagePockerAvatar({ uri }) {
+  return <Image source={uri ? { uri } : undefined} />
+}
 
-    const [response, setResponse] = React.useState(null);
+export const WelcomeScreen: FC<StackScreenProps<NavigatorParamList, "welcome">> = observer(() => {
+  const [response, setResponse] = React.useState<any>(null)
 
-    const 카메라실행 = React.useCallback((type, options) => {
-      if (type === 'capture') {
-        ImagePicker.launchCamera(options, setResponse);
-      } else {
-        ImagePicker.launchImageLibrary(options, setResponse);
-      }
-
-    }, [])
-
-    /**
-     * check 방식은 특정 권한상태확인후 리턴
-     * request 는 메시지를 띄움
-     * 
-     */
-    const requestPermisstion = () => {
-      request(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE).then(response => {
-        console.log(response);
-
-      })
-    }
-
-    const checkPermission = () => {
-      check(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE).then(result => {
-        switch (result) {
-          case RESULTS.UNAVAILABLE:
-            // 이 기능을 사용할 수 없습니다(이 장치에서 / 이 컨텍스트에서)
-            break;
-          case RESULTS.DENIED:
-            // 권한이 요청되지 않았습니다/거부되었지만 요청 가능합니다
-            break;
-          case RESULTS.GRANTED:
-            // 권한이 부여됨
-            break;
-          case RESULTS.LIMITED:
-            // 권한이 부여되지만 제한이 있습니다.
-            break;
-          case RESULTS.BLOCKED:
-          // 권한이 거부되었으며 더 이상 요청할 수 없습니다.
+  const onButtonPress = useCallback((type, options) => {
+    if (type === "capture") {
+      launchCamera(options, (res) => {
+        setResponse(res)
+        if (res?.errorCode === "permission") {
+          Alert.alert(
+            "경고",
+            "카메라 권한 설정이 필요합니다",
+            [
+              {
+                text: "설정하기",
+                onPress: () => Linking.openSettings(),
+              },
+              {
+                text: "Cancel",
+                onPress: () => console.log("취소 클릭"),
+                style: "cancel",
+              },
+            ],
+            {
+              cancelable: true,
+              onDismiss: () =>
+                Alert.alert("This alert was dismissed by tapping outside of the alert dialog."),
+            },
+          )
         }
-
       })
-
+    } else {
+      launchImageLibrary(options, setResponse).then((response) => {
+        // console.log(response)
+        const uri = response?.assets && response.assets[0].uri
+        // console.log(uri) // 로컬 파일 주소지
+      })
+      // console.log(response)
     }
+  }, [])
 
-
-    return (
-      <View testID="WelcomeScreen" style={FULL}>
-        <GradientBackground colors={["#422443", "#281b34"]} />
-        <Screen style={CONTAINER} preset="scroll" backgroundColor={color.transparent}>
-          <Header headerTx="welcomeScreen.poweredBy" style={HEADER} titleStyle={HEADER_TITLE} />
-          <Text style={TITLE_WRAPPER}>
-            접근 권한 승인
-          </Text>
-          <Text style={TITLE_WRAPPER}>
-            해당 서비스 이용을 위한 접근 권한을 허용합니다.
-          </Text>
-          <Text style={TITLE_WRAPPER}>
-            카메라 접근 권한을 허용합니다
-          </Text>
-        </Screen>
-        <SafeAreaView style={FOOTER}>
-          <View style={FOOTER_CONTENT}>
-            <Button
-              testID="next-screen-button"
-              style={CONTINUE}
-              textStyle={CONTINUE_TEXT}
-              tx="welcomeScreen.continue"
-              onPress={() => requestPermisstion()}
-            />
+  return (
+    <View testID="WelcomeScreen" style={FULL}>
+      <GradientBackground colors={["#422443", "#281b34"]} />
+      <Screen style={CONTAINER} preset="scroll" backgroundColor={color.transparent}>
+        <Header headerTx="welcomeScreen.poweredBy" style={HEADER} titleStyle={HEADER_TITLE} />
+        <Text style={TITLE_WRAPPER}>접근 권한 승인</Text>
+        <Text style={TITLE_WRAPPER}>해당 서비스 이용을 위한 접근 권한을 허용합니다.</Text>
+        <Text style={TITLE_WRAPPER}>카메라 접근 권한을 허용합니다</Text>
+        {response?.assets &&
+          response?.assets.map(({ uri }) => (
+            <View key={uri} style={IMAGE}>
+              <Image
+                resizeMode="cover"
+                resizeMethod="scale"
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{ width: 200, height: 200 }}
+                source={{ uri }}
+              />
+            </View>
+          ))}
+      </Screen>
+      <SafeAreaView style={FOOTER}>
+        <View style={FOOTER_CONTENT}>
+          <View style={BUTTONSTYLEL}>
+            {actions.map(({ title, type, options }) => {
+              return (
+                <Button
+                  key={title}
+                  text={title}
+                  onPress={() => onButtonPress(type, options)}
+                ></Button>
+              )
+            })}
           </View>
-        </SafeAreaView>
-      </View >
-    )
+        </View>
+      </SafeAreaView>
+    </View>
+  )
+})
+interface Action {
+  title: string
+  type: "capture" | "library"
+  options: ImagePicker.CameraOptions | ImagePicker.ImageLibraryOptions
+}
+
+const actions: Action[] = [
+  {
+    title: "Take Image",
+    type: "capture",
+    options: {
+      saveToPhotos: true,
+      mediaType: "photo",
+      includeBase64: false,
+      includeExtra,
+    },
   },
-)
+  {
+    title: "Select Image",
+    type: "library",
+    options: {
+      maxHeight: 200,
+      maxWidth: 200,
+      selectionLimit: 0,
+      mediaType: "photo",
+      includeBase64: false,
+      includeExtra,
+    },
+  },
+]
